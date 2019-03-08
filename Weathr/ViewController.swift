1964a716b6b90d2b4d0f8c8669c499986b7ae95c
Maroš Kováč <maros1305@gmail.com>
2019-03-08 17:15:38 +0100

    Initial Commit

---
+++ Weathr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weathr
+//
+//  Created by Maroš Kováč on 08/03/2019.
+//  Copyright © 2019 Maroš Kováč. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
